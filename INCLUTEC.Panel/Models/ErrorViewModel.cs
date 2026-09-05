b699cefdbb5f37480766ad249c3f538029a0112a
Emanuel Moyano <59158847+emamoyano23@users.noreply.github.com>
2026-09-04 22:29:35 -0300

    1.0.0

---
+++ INCLUTEC.Panel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INCLUTEC.Panel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
