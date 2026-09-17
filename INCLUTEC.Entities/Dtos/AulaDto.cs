27fec97f088ea382cfa3698e7411754bea001f3e
Rolando Brandan <106077950+rolandobrandan@users.noreply.github.com>
2026-09-17 18:15:20 -0300

    creacion de AulaDto

---
+++ INCLUTEC.Entities/Dtos/AulaDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace INCLUTEC.Entities.Dtos
+{
+    internal class AulaDto
+    {
+        public int Id {  get; set; }
+
+        public string? Nombre { get; set; }
+
+        public DateOnly AñoLectivo { get; set; }
+
+        public int ResponsableId { get; set; }
+
+        public bool EstadoActivo { get; set; }
+
+        
+    }
+}
